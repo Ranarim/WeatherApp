754a583df3519179c002c09e7208f3e8fba08999
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-03-26 20:05:04 +0100

    My changes from the weekend

--- server.js
+++ server.js
@@ -1,7 +1,51 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {}
+
+// Express to run server and routes
 const express = require("express");
-const bodyParser = require("body-parser")
+
+// Start up an instance of app
 const app = express();
 
+/* Dependencies */
+/* Middleware*/
+const bodyParser = require("body-parser")
+const cors = require("cors");
+
+
+//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => console.log("Webhook Server is listening on port 3000"))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Cors for cross origin allowance
+app.use(cors())
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Spin up the server
+const port = 3000;
+
+// Callback to debug
+app.listen(port, () => console.log(`Webhook Server is listening on port ${port}`))
+
+// Initialize all route with a callback function
+app.get("/all", sendData)
+app.post("/all", postData)
+
+// Callback function to complete GET '/all'
+function sendData(req, res) {
+    req.send(projectData);
+}
+
+// Post Route
+function postData(req, res) {
+    const data = req.body;
+    data.temperature = projectData.temperature;
+    data.date = projectData.date;
+    data.uresponse = projectData.uresponse;
+    res.send(projectData)
+}
+
+// Importing the Express librabry in the project
+// Creating instantiates inside the app variable
--- website/app.js
+++ website/app.js
@@ -1,5 +1,90 @@
-/* Global Variables */
+/* Global Variables 
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const { get } = require("http");
+const { getEnvironmentData } = require("worker_threads");
+ */
+
+/*Write an async function in app.js that uses fetch() to make a GET request to the OpenWeatherMap API. */
+/* Inside that callback function call your async GET request with the parameters:base url, user entered zip code (see input in html with id zip), personal API key*/
+async function getData() {
+    try {
+        const zipCode = document.getElementById("zip").value;
+        const personalAPIKey = '532d4540aaa725b5502a16c2c292e664';
+        const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${zipCode},de&appid=${personalAPIKey}&units=metric `
+        const fetchedData = await fetch(baseURL);
+        const weatherData = await fetchedData.json();
+        console.log(weatherData);
+        return weatherData
+    } catch (error) {
+        console.log(error, "getting data didnt work!");
+    }
+}
+
+/*Create an event listener for the element with the id: generate, with a callback function to execute when it is clicked.*/
+const generateBtn = document.getElementById("generate");
+generateBtn.addEventListener("click", getData);
+
+/*You will need to write another async function to make this POST request.*/
+async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    try {
+        const JSONData = response.json();
+        console.log(JSONData);
+        return JSONData;
+    } catch (error) {
+        console.log(error, "posting Data didnt work")
+    }
+}
+
+async function updateUI() {
+    try {
+        const response = await getData();
+        const dateElement = document.getElementById("date");
+        const tempElement = document.getElementById("temp");
+        const contentElement = document.getElementById("content");
+        dateElement.innerHTML = response.date;
+        tempElement.innerHTML = `${response.temp}`;
+        contentElement.innerHTML = `${response.content}`;
+    } catch (error) {
+        console.log(error, "updating UI didn't work")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Personal API Key for OpenWeatherMap API
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+/* Function to GET Project Data */
